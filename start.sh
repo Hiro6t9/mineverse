364377c5ddfc7db434124904a7c03f64c5cdb963
Hiro6t9 <hiroyt1137@gmail.com>
2024-11-01 18:37:58 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
-# Download and extract Java 17
-wget https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz
-tar -xzvf openjdk-17_linux-x64_bin.tar.gz
+# Server setup directory
+SERVER_DIR="/path/to/your/server"
+
+# Create Server Directory
+mkdir -p $SERVER_DIR
+cd $SERVER_DIR
+
+# Download and extract Java 18
+wget https://download.java.net/java/GA/jdk18/8c65bce9de504a57ab1ed170652f9718/31/GPL/openjdk-18_linux-x64_bin.tar.gz
+tar -xzvf openjdk-18_linux-x64_bin.tar.gz
 
 # Set environment variables
-export JAVA_HOME=$(pwd)/jdk-17
+export JAVA_HOME=$(pwd)/jdk-18
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Update the system alternatives
 sudo update-alternatives --install /usr/bin/java java $JAVA_HOME/bin/java 1
 sudo update-alternatives --install /usr/bin/javac javac $JAVA_HOME/bin/javac 1
 
-# Download server JAR
+# Download server JAR if not present
 SERVER_JAR_LINK="https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/365/downloads/paper-1.20.4-365.jar"
+if [ ! -f "$SERVER_DIR/server.jar" ]; then
 wget "$SERVER_JAR_LINK" -O server.jar
+fi
 
 # Accept the Minecraft EULA
+if [ ! -f "$SERVER_DIR/eula.txt" ]; then
 echo "eula=true" > eula.txt
+fi
 
-# Modify server.properties to set online-mode to false
+# Modify server.properties to set online-mode to false if not set
+if [ ! -f "$SERVER_DIR/server.properties" ] || ! grep -q "online-mode=false" "$SERVER_DIR/server.properties"; then
 echo "online-mode=false" >> server.properties
+fi
 
-# Launch the Minecraft server
-java -Xmx1G -Xms1G -jar server.jar nogui
+# Automatically detect and set max RAM
+TOTAL_MEM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+MAX_RAM=$((TOTAL_MEM / 1024))M
+
+# Launch the Minecraft server with maximum available RAM
+java -Xmx$MAX_RAM -Xms1G -jar server.jar nogui
